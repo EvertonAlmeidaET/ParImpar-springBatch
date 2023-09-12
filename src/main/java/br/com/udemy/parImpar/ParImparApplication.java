58cdd4c8ed6dc767db8daf2132a6a0509e64f85a
Everton de Almeida <everton17@outlook.com>
2023-09-12 01:58:31 -0300

    first commit

---
+++ src/main/java/br/com/udemy/parImpar/ParImparApplication.java
@@ -0,0 +1,13 @@
+package br.com.udemy.parImpar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParImparApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParImparApplication.class, args);
+	}
+
+}
